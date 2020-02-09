85cbe0ae0914bfaf3356230307619564b5b039a0
lllvcs <22395524+lllvcs@users.noreply.github.com>
2020-02-09 18:42:06 +0800

    new

--- README.md
+++ README.md
@@ -1,36 +1,43 @@
 # huaweicloud_ddns  华为云ddns脚本
 
+## 本脚本仅适用于ipv4地址的更新，ipv6版请查看```ipv6```分支
+## 网卡地址获取仅适用于Debian9+/Ubuntu18+（旧版本网卡地址获取请参考old分支）
+## 选择网卡获取ipv4地址时，请确认代码第88 90行处sed的行数
+## 请确认服务器的地域，并合适选择EndPoint地址
+
 ## 安装
-ubuntu/debian
+Ubuntu/Debian
 ```
 apt-get update
-apt-get install wget curl dnsutils openssl cron -y
-wget -N --no-check-certificate https://raw.githubusercontent.com/lllvcs/huaweicloud_ddns/master/getid.sh
+apt-get install wget curl dnsutils cron -y
 wget -N --no-check-certificate https://raw.githubusercontent.com/lllvcs/huaweicloud_ddns/master/huaweicloud_ddns.sh
+OR
+wget -N --no-check-certificate https://gitee.com/lvcs/huaweicloud_ddns/raw/master/huaweicloud_ddns.sh
 chmod +x ./huaweicloud_ddns.sh
-chmod +x ./getid.sh
 ```
 
-centos
+Centos
 ```
-yum install wget curl bind-utils openssl cron -y
-wget -N --no-check-certificate https://raw.githubusercontent.com/lllvcs/huaweicloud_ddns/master/getid.sh
+yum install wget curl bind-utils cron -y
 wget -N --no-check-certificate https://raw.githubusercontent.com/lllvcs/huaweicloud_ddns/master/huaweicloud_ddns.sh
+OR
+wget -N --no-check-certificate https://gitee.com/lvcs/huaweicloud_ddns/raw/master/huaweicloud_ddns.sh
 chmod +x ./huaweicloud_ddns.sh
-chmod +x ./getid.sh
 ```
 
 ## 首次操作
 第一步，先在DNS管理控制台```https://console.huaweicloud.com/dns/```内添加对应域名解析记录
 
-第二步，在```getid.sh```内按照提示填写相应信息。运行```getid.sh```，获取需要解析域名的```ZONE_ID```和```RECORDSET_ID```
+第二步，在```huaweicloud_ddns.sh```内填写 ```账号信息``` ```域名信息```
 
-第三步，在```huaweicloud_ddns.sh```内填写 ```ZONE_ID``` ```RECORDSET_ID```  ```ACCESS_KEY``` ```ACCESS_SECRET```
-
-第四步，运行```huaweicloud_ddns.sh```，设置定时任务
+第三步，运行```huaweicloud_ddns.sh```，设置定时任务
 
 ## 设置定时任务
 ```
 crontab -e
-*/1 * * * * bash /root/huaweicloud_ddns.sh
+* * * * * bash /root/huaweicloud_ddns.sh
 ```
+
+## 一点说明
+华为云目前虽然支持AK/SK调用API进行域名更新，但是在获取```Zone_ID```和```Record_ID```时需要有一个```X-Auth-Token```头的请求，而目前只能通过用户名、账户名和密码三者来获取```X-Auth-Token```，通过AK/SK获取```X-Auth-Token```目前只在华为内部实现，暂不对外开放。
+附上获取```Token```的PDF说明文档
--- huaweicloud_ddns.sh
+++ huaweicloud_ddns.sh
@@ -1,160 +1,107 @@
 #!/bin/bash
 stty erase ^h
-#ZONE_ID和RECORDSET_ID，分别为域名id和主机id，请通过 get_id.sh 或官方api获取 https://dns.myhuaweicloud.com/v2/zones 
-#ACCESS_KEY和ACCESS_SECRET请在 我的凭证-访问密钥中生成
-#详情请参考官方文档 https://support.huaweicloud.com/dns/index.html 
+clear
+# Author LVCS
+# https://github.com/lllvcs/huaweicloud_ddns
+# https://gitee.com/lvcs/huaweicloud_ddns
+#在运行此脚本之前，请先在DNS管理控制台内添加对应域名A记录！
+#Please add the corresponding domain name record in the DNS management console before running this shell script!
+#一般来说用户名和账户名相同
 
-ZONE_ID="ZONE_ID"
-RECORDSET_ID="RECORDSET_ID"
+#用户名
+username=""
+#账户名
+accountname=""
+#密码
+password=""
 
-ACCESS_KEY="ACCESS_KEY"
-ACCESS_SECRET="ACCESS_SECRET"
-
-#从外网api获取ip地址(默认开启)
-REMOTE_RESOLVE=1
+#域名
+domain="example.com"
+#主机名
+host="www"
 
 #获取ip地址网址
-GETIPURL="https://api.ip.la"
-#GETIPURL="http://ip.sb"
+GETIPURL="ip.sb"
 #GETIPURL="http://ip.6655.com/ip.aspx"
 #GETIPURL="http://ip.3322.net"
 #GETIPURL="http://members.3322.org/dyndns/getip"
 
-#或从网卡获取ip地址(填写网卡名)
+#从外网api获取ip地址(默认开启1)
+REMOTE_RESOLVE=1
+
+#从网卡获取ip地址(填写网卡名 如eth0 ens3)
+#并请根据实际情况填写sed行数(第88 90行处)
 INTERFACE=""
 
 TARGET_IP=""
 
-#Debug模式(默认关闭)
-DEBUG=0
+#End Point 终端地址 请根据地域选择
+iam="iam.myhuaweicloud.com"
+#iam="iam.ap-southeast-1.myhuaweicloud.com"
+#iam="iam.ap-southeast-3.myhuaweicloud.com"
 
-usage() {
-cat<<EOF
-Usage: 
-    ./HuaweiCloud-DDNS.sh --access-key "ACCESS_KEY" --access-secret "ACCESS_SECRET" -z "ZONE-ID" -r "RECORDSET_ID" -i "INTERFACE" -m
+dns="dns.myhuaweicloud.com"
+#dns="dns.ap-southeast-1.myhuaweicloud.com"
+#dns="dns.ap-southeast-1.myhuaweicloud.com"
 
-    -k|--access-key
-    -s|--access-secret
-    -z|--zone-id
-    -r|--recordset-id
-    -i|--interface
-    -m|--remote-resolve
-    --address <ip address>
-    -v|--debug
-    -h|--help
-EOF
-	exit 1; 
-}
 
-ARGS=`getopt -o k:s:z:r:i:mvh -l access-key:,access-secret:,zone-id:,recordset-id:,interface:,remote-resolve,debug,help,address: -n HuaweiCloud-DDNS.sh -- "$@"`
-if [ $? != 0 ]; then
-    usage
-    exit 1
-fi
+token_X="$(
+    curl -L -k -s -D - -X POST \
+        "https://$iam/v3/auth/tokens" \
+        -H 'content-type: application/json' \
+        -d '{
+    "auth": {
+        "identity": {
+            "methods": ["password"],
+            "password": {
+                "user": {
+                    "name": "'$username'",
+                    "password": "'$password'",
+                    "domain": {
+                        "name": "'$accountname'"
+                    }
+                }
+            }
+        },
+        "scope": {
+            "domain": {
+                "name": "'$accountname'"
+            }
+        }
+    }
+  }' | grep X-Subject-Token
+)"
 
-eval set -- "$ARGS"
+token="$(echo $token_X | awk -F ' ' '{print $2}')"
 
-while true; do
-	case "$1" in
-		-k|--access-key)
-			ACCESS_KEY=$2
-			shift
-			;;
-		-s|--access-secret)
-			ACCESS_SECRET=$2
-			shift
-			;;
-		-z|--zone-id)
-			ZONE_ID=$2
-			shift
-			;;
-        -r|--recordset-id)
-            RECORDSET_ID=$2
-			shift
-			;;
-		-i|--interface)
-			INTERFACE=$2
-			shift
-			;;
-		-m|--remote-resolve)
-			REMOTE_RESOLVE=1
-			;;
-		--address)
-			TARGET_IP=$2
-			shift
-			;;
-		-v|--debug)
-			DEBUG=1
-			;;
-		-h|--help)
-			usage
-			exit 1
-			;;
-		--)
-			shift
-			break
-			;;
-		*)
-			usage
-			exit 1
-			;;
-	esac
-shift
-done
+recordsets="$(
+    curl -L -k -s -D - \
+        "https://$dns/v2/recordsets?name=$host.$domain." \
+        -H 'content-type: application/json' \
+        -H 'X-Auth-Token: '$token | grep -o "id\":\"[0-9a-z]*\"" | awk -F : '{print $2}' | grep -o "[a-z0-9]*"
+)"
+
+RECORDSET_ID=$(echo $recordsets | cut -d ' ' -f 1)
+ZONE_ID=$(echo $recordsets | cut -d ' ' -f 2 | cut -d ' ' -f 2)
 
 if [ -z $TARGET_IP ]; then
-	if [ $REMOTE_RESOLVE -eq 1 ]; then
-		if [ $DEBUG -eq 1 ]; then
-			if [ $INTERFACE ]; then
-				TARGET_IP=$(curl --interface $INTERFACE $GETIPURL)
-			else
-				TARGET_IP=$(curl $GETIPURL)
-			fi
-		else
-			if [ $INTERFACE ]; then
-				TARGET_IP=$(curl -s --interface $INTERFACE $GETIPURL)
-			else
-				TARGET_IP=$(curl -s $GETIPURL)
-			fi
-		fi	
-	else
-		if [ $INTERFACE ]; then
-			TARGET_IP=$(ifconfig $INTERFACE | grep 'inet addr' | awk -F ":" '{print $2}' | awk '{print $1}')
-		else
-			TARGET_IP=$(ifconfig | grep 'inet addr' | awk -F ":" '{print $2}' | awk '{print $1}' | head -n 1)
-		fi
-	fi
+    if [ $REMOTE_RESOLVE -eq 1 ]; then
+        if [ $INTERFACE ]; then
+            TARGET_IP=$(curl -s -4 --interface $INTERFACE $GETIPURL)
+        else
+            TARGET_IP=$(curl -s -4 $GETIPURL)
+        fi
+    else
+        if [ $INTERFACE ]; then
+            TARGET_IP=$(ifconfig $INTERFACE | grep 'inet ' | cut -d 't' -f 2 | cut -d ' ' -f 2 )
+        else
+            TARGET_IP=$(ifconfig | grep 'inet ' | cut -d 't' -f 2 | cut -d ' ' -f 2 | sed -n 1p )
+        fi
+    fi
 fi
 
-if [ $DEBUG -eq 1 ]; then
-cat<<EOF
-HuaweiCloud DDNS script debug info.
-    ACCESS KEY: $ACCESS_KEY
- ACCESS SECRET: $ACCESS_SECRET
-       ZONE ID: $ZONE_ID
-  RECORDSET ID: $RECORDSET_ID
-     Interface: $INTERFACE
-Remote Resolve: $REMOTE_RESOLVE
-    Debug Mode: $DEBUG
-EOF
-fi
-
-REQUEST_METHOD="PUT"
-REQUEST_HOST="dns.myhuaweicloud.com"
-REQUEST_PATH="/v2/zones/$ZONE_ID/recordsets/$RECORDSET_ID"
-REQUEST_PARAM=""
-REQUEST_DATA="{\"records\":[\"$TARGET_IP\"]}"
-
-REQUEST_CONTENT_TYPE='application/json'
-REQUEST_TIME=`date -u +%Y%m%dT%H%M%SZ`
-
-REQUEST_DATA_HASH=`echo -ne "$REQUEST_DATA" | openssl dgst -sha256 -hex | grep -Po "([0-9a-f]{64})"`
-REQUEST_HEADER="$REQUEST_METHOD\n$REQUEST_PATH/\n$REQUEST_PARAM\ncontent-type:$REQUEST_CONTENT_TYPE\nhost:$REQUEST_HOST\nx-sdk-date:$REQUEST_TIME\n\ncontent-type;host;x-sdk-date"
-REQUEST_HASH=`echo -ne "$REQUEST_HEADER\n$REQUEST_DATA_HASH" | openssl dgst -sha256 -hex | grep -Po "([0-9a-f]{64})"`
-HMAC=`echo -ne "SDK-HMAC-SHA256\n$REQUEST_TIME\n$REQUEST_HASH" | openssl dgst -sha256 -hmac "$ACCESS_SECRET" -hex | grep -Po "([0-9a-f]{64})"`
-
-if [ $DEBUG -eq 1 ]; then
-	curl -X $REQUEST_METHOD "https://$REQUEST_HOST$REQUEST_PATH?$REQUEST_PARAM" -H "Content-Type: $REQUEST_CONTENT_TYPE" -H "X-Sdk-Date: $REQUEST_TIME" -H "host: $REQUEST_HOST" -H "Authorization: SDK-HMAC-SHA256 Access=$ACCESS_KEY, SignedHeaders=content-type;host;x-sdk-date, Signature=$HMAC" -d $REQUEST_DATA
-else
-	curl -s -X $REQUEST_METHOD "https://$REQUEST_HOST$REQUEST_PATH?$REQUEST_PARAM" -H "Content-Type: $REQUEST_CONTENT_TYPE" -H "X-Sdk-Date: $REQUEST_TIME" -H "host: $REQUEST_HOST" -H "Authorization: SDK-HMAC-SHA256 Access=$ACCESS_KEY, SignedHeaders=content-type;host;x-sdk-date, Signature=$HMAC" -d $REQUEST_DATA
-fi
+curl -X PUT -L -k -s \
+    "https://$dns/v2/zones/$ZONE_ID/recordsets/$RECORDSET_ID" \
+    -H "Content-Type: application/json" \
+    -H "X-Auth-Token: $token" \
+    -d "{\"records\": [\"$TARGET_IP\"],\"ttl\": 1}"
