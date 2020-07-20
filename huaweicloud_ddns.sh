0b6b96dcc360bc8777cdace76523ee66a8290d50
lllvcs <22395524+lllvcs@users.noreply.github.com>
2020-07-21 01:08:53 +0800

    Update huaweicloud_ddns.sh

--- huaweicloud_ddns.sh
+++ huaweicloud_ddns.sh
@@ -21,12 +21,14 @@ domain="example.com"
 host="www"
 
 #获取ip地址网址
-GETIPURL="ip.sb"
+GETIPURL="whatismyip.akamai.com"
+#GETIPURL="ip.sb"
 #GETIPURL="api.myip.la"
 #GETIPURL="http://v4.ip.zxinc.org/getip"
 #GETIPURL="http://ip.6655.com/ip.aspx"
 #GETIPURL="http://ip.3322.net"
 #GETIPURL="http://members.3322.org/dyndns/getip"
+
 
 #从外网api获取ip地址(默认开启1)
 REMOTE_RESOLVE=1
